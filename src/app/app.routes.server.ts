d2e0adfe096a85f1df4cbc9293c275b75c285bab
DESKTOP-RE0DLQK\J3r1ck <signapan.jerick27@gmail.com>
2026-04-01 16:51:28 +0800

    fix: streamline server routes by consolidating render mode definitions

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,22 +1,12 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: 'tickets/edit/:id',
-    renderMode: RenderMode.Client,
-  },
-  {
-    path: 'assets/:id',
-    renderMode: RenderMode.Client,
-  },
-  {
-    path: 'employees/edit/:id',
-    renderMode: RenderMode.Client,
-  },
-  {
-    path: 'brands/edit/:id',
-    renderMode: RenderMode.Client,
-  },
+  { path: 'tickets/:id', renderMode: RenderMode.Client },
+  { path: 'tickets/edit/:id', renderMode: RenderMode.Client },
+  { path: 'assets/:id', renderMode: RenderMode.Client },
+  { path: 'employees/:id', renderMode: RenderMode.Client },
+  { path: 'employees/edit/:id', renderMode: RenderMode.Client },
+  { path: 'brands/edit/:id', renderMode: RenderMode.Client },
   {
     path: '**',
     renderMode: RenderMode.Prerender,
